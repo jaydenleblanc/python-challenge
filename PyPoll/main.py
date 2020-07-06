dea238caf905a8809c62912a7b2bdd3d9c2ae039
Jayden LeBlanc <jaydenleblanc@gmail.com>
2020-07-05 23:59:07 -0400

    Text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,15 +32,14 @@ with open(csvpath) as csvfile:
 
 total_votes_cast = (len(voters))
 
-
 #Complete List of Candidates who received votes (return unique values only using 'set' data type)
 candidates_set = set(candidates)
 new_candidates_list = list(candidates_set)
 
-#Count number of votes for each candidate (how many times the name appears in the list)
+#Count number of votes for each candidate (how many times the name appears in the list) Result is a dictionary
 votes_counted = Counter(candidates)
 
-#Find Candidate with the most votes
+#Find Candidate with the most votes by converting to a list
 winner = max(votes_counted.values())
 final_list = [i for i in votes_counted.keys() if votes_counted[i]== winner]
 
@@ -59,6 +58,23 @@ print("------------------------------------")
 print("The Winner of the Election is:")
 print(sorted(final_list)[0])
 
+
+
+#Print to text file
+f = open("finalresults.txt", "a")
+print("Election Results", file=f)
+print("-----------------------------------", file=f)
+print(f"Total Number of votes cast: {total_votes_cast}", file=f)
+print("-----------------------------------", file=f)
+
+#Calculate Percentage for each candidate
+sum_votes_counted = sum(votes_counted.values())
+for k,v in votes_counted.items():
+    percentage = v * 100.0 / sum_votes_counted
+    print(f" {k}:{percentage}% ({v})", file=f)
+print("------------------------------------", file=f)
+print("The Winner of the Election is:", file=f)
+print(sorted(final_list)[0], file=f)
 
 
 
